8036dcdb00f8b6d302fcf9b1241714b6b62fead1
collector1871 <andakin.krk@gmail.com>
2016-01-04 22:31:45 +0100

    dalej

---
+++ dcmp/monitoring.py
@@ -0,0 +1,9 @@
+'''
+Created on 4 sty 2016
+'''
+
+from delimgconv import BierzUrl
+
+strona = "http://www.delcampe.net/page/item/id,259337337,var,Russia-Russie-Russland-ZEMSTVO-local-post-colection-19-stamps-from-19-diffrent-districts,language,E.html"
+
+BierzUrl(strona)
