686b74d7ff23fee4045ccadf35b14ac903858428
collector1871 <andakin.krk@gmail.com>
2016-01-04 23:58:53 +0100

    dalej

--- dcmp/monitoring.py
+++ dcmp/monitoring.py
@@ -5,6 +5,6 @@ Created on 4 sty 2016
 from delimgconv import BierzUrl
 from budowaStr import BierzUrl2
 
-strona = "http://www.delcampe.net/items?language=E&id_member=913749&cat=-2&searchString=&page=1&useAsDefault=N&layoutForm[listitemsperpage]=500"
+strona = ""
 
 BierzUrl2(strona)
