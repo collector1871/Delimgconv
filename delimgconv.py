cdb3d8209dfe00fe3010cab12d18f06776bda329
collector1871 <andakin.krk@gmail.com>
2016-01-04 21:35:08 +0100

    po 1 linii

--- delimgconv.py
+++ delimgconv.py
@@ -22,6 +22,7 @@ zupa = BeautifulSoup(zupa1, "html.parser")
 tags = zupa.findAll('img', style="width:99%;")
 koniec = str("\n".join(set(tag['src'] for tag in tags)))
 
+# wyswietlenie po 1 linii output - mozna pracowac z (lin)e
 koniec1 = StringIO(koniec)
 for line in koniec1:
     print(line)
