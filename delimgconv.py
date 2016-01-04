156fdeed1cbc87cd0a1e49fa454ff89b57976d4d
collector1871 <andakin.krk@gmail.com>
2016-01-04 20:45:58 +0100

    dalsza czesc

--- delimgconv.py
+++ delimgconv.py
@@ -6,7 +6,6 @@ Created on 4 sty 2016
 
 import urllib.request
 from bs4 import BeautifulSoup
-
 page = urllib.request.urlopen('http://www.delcampe.net/page/item/id,321490576,var,indigenas-mossumbes-editon-raul-peres-leiro-novo-redondo--mauvais-etat-timbre-decoupe--recto-verso-,language,E.html')
 
 zupa1 = page.read()
@@ -14,8 +13,4 @@ zupa1 = page.read()
 zupa = BeautifulSoup(zupa1, "html.parser")
 tags = zupa.findAll('img', style="width:99%;")
 
-# print "\n".join(tags)
-
-for word in tags:
-    print(word)
-# print(tags)
+print("\n".join(set(tag['src'] for tag in tags)))
